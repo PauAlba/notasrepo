25c283f4627c580b151fb9146e1ecd8c4fd50796
Paulina <paulinaorozcolsb@gmail.com>
2025-11-04 10:59:30 -0600

    notas views v2

--- notas/urls.py
+++ notas/urls.py
@@ -4,13 +4,7 @@ from . import views
 app_name = 'notas'
 
 urlpatterns = [
-  path("", views.lista, name="lista" ),
-  #/notas/3
-  path("<int:nota_id>/", views.detail, name='detail'),
-  path("create/", views.create, name="create"),
-  path("formulario/", views.formulario, name='formulario'),
-  path("<int:nota_id>/editarForms/", views.editarForms, name='editarForms'),
-  path("<int:nota_id>/update/", views.update, name="update"),
-  path("<int:nota_id>/delete/", views.delete, name="delete"), 
-
+    path("", views.ListaView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("formulario/", views.FormularioView.as_view(), name="formulario"),
 ]
